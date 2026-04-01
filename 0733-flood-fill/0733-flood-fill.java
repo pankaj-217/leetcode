df4908697edc5d16ff3fe0167c74e309a6b6c99e
PANKAJ GOND <pankajgond200@gmail.com>
2026-04-01 13:27:13 +0530

    Time: 0 ms (100%), Space: 46.8 MB (72.2%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+     int old_color=image[sr][sc];
+    if(image[sr][sc]==color){
+        return image;
+     }   
+     solve(image,sr,sc,color,old_color);
+     return image;
+    }
+    public void solve(int image[][],int cr ,int cc,int color,int old_color){
+        if(cr<0 || cc<0 || cr>=image.length || cc>=image[0].length || image[cr][cc]!=old_color){
+            return;
+        }
+        image[cr][cc]=color;
+        solve(image,cr-1,cc,color,old_color);
+        solve(image,cr+1,cc,color,old_color);
+        solve(image,cr,cc+1,color,old_color);
+        solve(image,cr,cc-1,color,old_color);
+    }
+}
